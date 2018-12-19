71f351826ec87eca4816432dca834d716b23c14d
Eddy <eddy.xu@orchestrallinc.cn>
2018-12-19 17:44:10 +0800

    更新

---
+++ src/main/java/com/xhg/studyelement/controller/UeditorController.java
@@ -0,0 +1,26 @@
+package com.xhg.studyelement.controller;
+
+import com.baidu.ueditor.ActionEnter;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.UnsupportedEncodingException;
+
+/**
+ * 用于处理ueditor插件相关请求
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/ueditor")
+public class UeditorController {
+    @RequestMapping(value = "/exec")
+    public String exec(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+        request.setCharacterEncoding("utf-8");
+        response.setHeader("Content-Type" , "text/html");
+        String rootPath = request.getServletContext().getRealPath("/");
+        return new ActionEnter(request, rootPath).exec();
+    }
+}
