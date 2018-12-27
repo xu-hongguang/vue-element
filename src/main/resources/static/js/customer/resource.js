7e9b688138aaafcdb30174e6b746533fe9991540
Eddy <eddy.xu@orchestrallinc.cn>
2018-12-27 10:10:25 +0800

    更新

--- src/main/resources/static/js/customer/resource.js
+++ src/main/resources/static/js/customer/resource.js
@@ -9,7 +9,7 @@ document.write('<script src="js/bootstrap.min.js"></script>');
 document.write('<script type="text/javascript" charset="utf-8" src="js/customer/weburi.js"></script> ');
 
 //国际化
-document.write('<script type="text/javascript" charset="utf-8" src="js/customer/i18n.js"></script> ');
+// document.write('<script type="text/javascript" charset="utf-8" src="js/customer/i18n.js"></script> ');
 
 //页面加载loading
 document.write('<script type="text/javascript" charset="utf-8" src="js/customer/loading.js"></script> ');
